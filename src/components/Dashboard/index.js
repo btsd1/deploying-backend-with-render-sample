facd54e9b1edcf584732cfda6d51ef9b6cd85143
btsd1 <108137547+btsd1@users.noreply.github.com>
2024-04-18 15:45:42 -0400

    adding frontend

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,12 @@
+import { Box, Grid } from '@mui/material';
+import React, { Fragment } from 'react';
+
+function Dashboard() {
+    return (
+        <Box mt={2}>
+            <h1>Hello World</h1>
+        </Box>
+    )
+}
+
+export default Dashboard;
